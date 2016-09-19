63d878c6564961bce720688e99fe9332bc6e80db
anbelespinal <anabelespinal>
2016-09-19 17:30:43 -0500

    limpiando codigo

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
-window.onload = function(){
-	var enviar = document.getElementById("enviar");
-	enviar.onclick = function(e){
+window.addEventListener("load", function(){
+	var boton = document.getElementById("enviar");
+	boton.addEventListener("click", function(e){
 		e.preventDefault();
-		var comentario = document.getElementById("comentario").value;       //setAtribute("class","comentarios");
-		var repo = document.createElement("div");
-		var textnode = document.createTextNode(comentario);
-		repo.appendChild(textnode);
-		var list = document.getElementById("contenedorComentarios");
-		list.insertBefore(repo, list.childNodes[0]);
-		document.getElementById("comentario").value = "";
-	}; 
-	var textArea = document.getElementById("comentario");
-	textArea.onmouseover = function(){
-		textArea.style.height = "60px";
+		var comentario = document.getElementById("comentario");
+		var valorComentario = comentario.value;
+		agregarMensaje(valorComentario);
+		valorComentario.value = "";
+	});
+
+	function agregarMensaje(valorComentario){
+		var div = document.createElement("div");
+		div.innerText = valorComentario;
+		var comentarios = document.getElementById("contenedorComentarios");
+		comentarios.insertBefore(div, comentarios.childNodes[0]);
 	};
-	textArea.onmouseout = function(){
-		textArea.style.height = "30px";
-	};
-}
+})
+
+
+
+
+// window.onload = function(){
+// 	var enviar = document.getElementById("enviar");
+// 	enviar.onclick = function(e){
+// 		e.preventDefault();
+// 		var comentario = document.getElementById("comentario").value;       //setAtribute("class","comentarios");
+// 		var repo = document.createElement("div");
+// 		var textnode = document.createTextNode(comentario);
+// 		repo.appendChild(textnode);
+// 		var list = document.getElementById("contenedorComentarios");
+// 		list.insertBefore(repo, list.childNodes[0]);
+// 		document.getElementById("comentario").value = "";
+// 	}; 
+// 	var textArea = document.getElementById("comentario");
+// 	textArea.onmouseover = function(){
+// 		textArea.style.height = "60px";
+// 	};
+// 	textArea.onmouseout = function(){
+// 		textArea.style.height = "30px";
+// 	};
+// }
