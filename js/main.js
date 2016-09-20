4720e75c8c0dd1ba1d5863e1e2d6cdfeb86e2bfd
anbelespinal <anabelespinal>
2016-09-20 13:17:12 -0500

    height-textarea

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", function(){
-
 	var boton = document.getElementById("enviar");
+	var comentario = document.getElementById("comentario");
+
 	boton.addEventListener("click", function(e){
 		e.preventDefault();
-		var comentario = document.getElementById("comentario");
 		var valorComentario = comentario.value;
 		agregarMensaje(valorComentario);
-
+		document.getElementById("contador").innerText = "140";
+		comentario.value = "";
 		boton.disabled = true;
 	});
 
@@ -20,26 +21,48 @@ window.addEventListener("load", function(){
 	comentario.addEventListener("keydown", function(){
 		boton.disabled = false;
     var max = 140;
-    var valorComentario = document.getElementById("comentario").value;
-    var longitud = valorComentario.length;
+    var longitud = comentario.value.length;
+    var contador = document.getElementById("contador");
     if(longitud <= max) { 
-        document.getElementById("contador").value = max-longitud; 
+        contador.innerText = max-longitud; 
       }else{
-        document.getElementById("contador").value = max -longitud;
+        contador.innerText = max -longitud;
     }
-    if(longitud == 120){
-    	document.getElementById("contador").setAttribute("class", "verde");
+    if(longitud >= 120){
+    	contador.setAttribute("class", "verde");
+    }else{
+    	contador.setAttribute("class", "negro");
     }
-    if(longitud == 130){
-    	document.getElementById("contador").setAttribute("class", "rojo");
+    if(longitud >= 130){
+    	contador.setAttribute("class", "rojo");
+    } 
+    if(longitud > 140){
+    	contador.setAttribute("class", "rojo");
+    	boton.disabled = "true";
+    } 
+    if (longitud == 0){
+  	  boton.disabled = true;
     }
-    // if (longitud == -1){
-  	 //  boton.disabled = true;
-    // }
-    // if(event.keyCode == 13){
-    // 	document.getElementById("comentario").style.height = "10px";
-    // }
- 	});
+    if (longitud == 140){
+  	   boton.disabled = true;   
+    }
+    if(event.keyCode == 13){
+    	var height = comentario.style.height;
+    	var incremento = height + "10px"
+    	document.getElementById("comentario").style.height = incremento;
+    }
+  });
+	var textarea = document.getElementById("comentario");
 
- // Al presionar enter ("/n") que crezca el textarea de acuerdo al tamaño del texto.
+  textarea.addEventListener('keydown', autosize);
+             
+	function autosize(){
+		var el = this;
+ setTimeout(function(){
+		el.style.cssText = 'height:auto; padding:0';
+    // for box-sizing other than "content-box" use:
+    // el.style.cssText = '-moz-box-sizing:content-box';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
+ }
 }); 
